7b49e69bbee4aaa4e614e834839c5505441c129a
Vincedacode <toluwanipromise2005@gmail.com>
2026-07-17 17:07:39 -0700

    Welcome screens done

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import ThemeProvider from "./src/context/themecontext";
 import { AuthProvider } from "./src/hooks/useAuth";
 import AppNavigator from "./src/navigation/AppNavigator";
 
+
+
 export default function App() {
   return (
     <AuthProvider>
